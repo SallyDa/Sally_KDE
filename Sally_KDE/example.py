d10f863b9b64e214bb6789f07c246676dea68f2d
Sally <sally.dacie.14@ucl.ac.uk>
2016-05-17 11:35:21 +0100

    Corrected error: changed map for which the kde best fit line is found to the submap rather than the whole Sun.

--- Sally_KDE/example.py
+++ Sally_KDE/example.py
@@ -1,24 +1,23 @@
 
-import sunpy.map
 import os.path
-import astropy.units as u
+import tempfile
+
+import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
-import numpy as np
+import sunpy.map
+import astropy.units as u
 
-code_directory = 'C:/Users/admin/Documents/kde_code'
-data_directory = 'D:/data/HMI/example'
+from Sally_KDE import radialise, kde, slope
 
-import sys
-sys.path.append(os.path.abspath(code_directory))
-from kde_code import radialise, kde, slope
+data_directory = tempfile.mkdtemp(prefix='KDE_example')
 
 ### download data ###
 from sunpy.net import vso
 client = vso.VSOClient()
 result = client.query(vso.attrs.Time((2015, 11, 3, 13, 30), (2015, 11, 3, 13, 31)), 
                       vso.attrs.Instrument('hmi'), vso.attrs.Physobs('LOS_magnetic_field'))
-download = client.get(result, path=os.path.join(data_directory, 'decaying_region.fits'))
+download = client.get(result, path=os.path.join(data_directory, 'decaying_region.fits')).wait()
 # Note: to save the file with its proper file name, use path=os.path.join(data_directory, '{file}')
 
 ### open data file, make submap and radialse ###
@@ -35,7 +34,7 @@ rsmap.plot(vmin=-200, vmax=200) # plot the submap
 ax2 = plt.subplot(gs[1])
 kde1 = kde.kde_plot(rsmap, ax=ax2) # plot the kde
 # to find the knees, calculate first the extra smooth kde
-kde2data = kde.kde_plot(mymap, ax=None, extrasmooth=2)[0]
+kde2data = kde.kde_plot(rsmap, ax=None, extrasmooth=2)[0]
 slope2 = slope.slope_plot(kde2data) # find the slope of the kde plot
 # find the knee positions, based on the extra smoothed kde data
 knee1p = slope.B_at_steepest_slope(slope.cut_slope_array(slope2[1], cutoff=-1))
@@ -60,10 +59,10 @@ print('positive slope {}, negative slope {}'.format(slopep, slopen))
 # plot vertical lines to indicate the position of the knees
 fvals1 = np.asarray([10**-6, 10**-1])
 for kneep in [knee1p, knee2p]:
-    v_pos = np.asarray([1, 1]*kneep)
+    v_pos = np.array([1, 1])*kneep
     ax2.plot(v_pos, fvals1, color='r', alpha=0.3)
 for kneen in [knee1n, knee2n]:
-    v_neg = np.asarray([1, 1]*kneen)
+    v_neg = np.array([1, 1])*kneen
     ax2.plot(v_neg, fvals1, color='b', alpha=0.3)
 plt.tight_layout()  
 plt.show()
